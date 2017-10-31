eaced829306da1e67e8754dc9e5b31614f38c6d5
Matt Hayes <matt@mysterycommand.com>
2017-10-31 18:46:54 -0400

    build(webpack): adds tslint-loader and configures it for webpack (base)

--- config/webpack/base.ts
+++ config/webpack/base.ts
@@ -2,6 +2,15 @@ import { Configuration } from 'webpack';
 
 const config: Configuration = {
   entry: ['./source/index.ts'],
+  module: {
+    rules: [
+      {
+        enforce: 'pre',
+        loader: 'tslint-loader',
+        test: /\.ts$/,
+      },
+    ],
+  },
   output: {
     filename: './static/main.js',
     pathinfo: true,
