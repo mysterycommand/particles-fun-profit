2d72024a3108e0e1ad7901eeed046b63deb5f37f
Matt Hayes <matt@mysterycommand.com>
2017-11-04 12:53:12 -0400

    chore: fuck it

--- source/app/state.ts
+++
@@ -1,4 +0,0 @@
-export interface State {
-  currentTime: number;
-  deltaTime: number;
-}
--- tsbuild/source/app/update.js
+++
@@ -1,7 +0,0 @@
-export default function update(currentTime, deltaTime) {
-    return {
-        currentTime,
-        deltaTime,
-    };
-}
-//# sourceMappingURL=update.js.map
