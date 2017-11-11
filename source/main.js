a91bd91c66ede8e8d93fa64f31eb76781be88980
Matt Hayes <matt@mysterycommand.com>
2017-11-11 14:54:56 -0500

    refactor: it looks like fireworks now

--- source/main.js
+++ source/main.js
@@ -1,64 +1,28 @@
-import { cos, ππ, random, /* saw, */ sin } from './util/math';
-// import { getWaveFn } from './util/wave';
+import { cos, ππ, random, sin } from './util/math';
 
 import gameLoop from './lib/game-loop';
 import objectPool from './lib/object-pool';
 
-import { canvas, w, h } from './app/canvas';
+import { w, h } from './app/canvas';
 import render from './app/render';
+import { setTimeout } from 'timers';
 
-let mouseDown = false;
-let mouseX = w / 2;
-let mouseY = h / 2;
+let shouldActivate = false;
+let activeX = w / 2;
+let activeY = h / 2;
 
-function onMouseDown(event) {
-  event.preventDefault();
-  event.stopPropagation();
-  mouseDown = true;
-}
-
-function onMouseUp(event) {
-  event.preventDefault();
-  event.stopPropagation();
-  mouseDown = false;
-}
-
-function onMouseMove(event) {
-  event.preventDefault();
-  event.stopPropagation();
-  mouseX = event.offsetX;
-  mouseY = event.offsetY;
-}
-
-function onMouseEnter(event) {
-  event.preventDefault();
-  event.stopPropagation();
-  canvas.addEventListener('mousemove', onMouseMove);
-}
-
-function onMouseLeave(event) {
-  event.preventDefault();
-  event.stopPropagation();
-  canvas.removeEventListener('mousemove', onMouseMove);
-  mouseX = w / 2;
-  mouseY = h / 2;
-}
-
-window.addEventListener('mousedown', onMouseDown);
-window.addEventListener('mouseup', onMouseUp);
-
-canvas.addEventListener('mouseenter', onMouseEnter);
-canvas.addEventListener('mouseleave', onMouseLeave);
-
-const pool = objectPool(5000);
+const drag = 0.95;
+const grav = 0.2;
+const fade = 0.995;
 
 function reset(p) {
-  p.px = mouseX;
-  p.py = mouseY;
+  p.px = activeX;
+  p.py = activeY;
   p.alpha = 1;
 
   const theta = random() * ππ;
   const radius = 10 + random() * 20;
+
   p.vx = radius * cos(theta);
   p.vy = radius * sin(theta);
 }
@@ -69,23 +33,19 @@ function isInBounds({ px, py }) {
   return vertical && horizontal;
 }
 
-const drag = 0.9;
-const grav = 0.4;
-const fade = 0.9;
-
-// initialize
+const pool = objectPool(5000);
 pool.initialize(reset);
 
 function game(currentTime, deltaTime) {
-  let count = 0;
-  let total = mouseDown ? 50 : 0;
+  let numActivated = 0;
+  let numToActivate = shouldActivate ? 100 : 0;
 
   pool.update(p => {
     // activate
-    if (count < total && !p.active) {
+    if (numActivated < numToActivate && !p.active) {
       reset(p);
       p.active = true;
-      count++;
+      numActivated++;
     }
 
     // update
@@ -108,14 +68,14 @@ function game(currentTime, deltaTime) {
   });
 }
 
+function boom() {
+  setTimeout(boom, 1000 + random() * 1000);
+  shouldActivate = !shouldActivate;
+  setTimeout(() => (shouldActivate = !shouldActivate), 50);
+  activeX = w * 0.2 + random() * (w * 0.6);
+  activeY = h * 0.2 + random() * (h * 0.6);
+}
+
 const loop = gameLoop(game);
 loop.start();
-// loop.goto(0);
-// for (let i = 0; i < 10; ++i) {
-//   loop.goto(i);
-// }
-// // render
-// render({
-//   particles: pool.active,
-//   deltaTime: 1000 / 60,
-// });
+boom();
