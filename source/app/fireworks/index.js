c608180802f7c799e491e21f766b799126887637
Matt Hayes <matt@mysterycommand.com>
2017-11-16 13:36:21 -0500

    refactor: use mouse position instead of interval

--- source/app/fireworks/index.js
+++ source/app/fireworks/index.js
@@ -1,5 +1,5 @@
 import { random } from '../../util/math';
-import { w, h } from '../canvas';
+import { w, h, target } from '../canvas';
 
 import boomField from './fields/boom';
 export { default as render } from './render';
@@ -8,16 +8,37 @@ let shouldBoom = false;
 let boomX = w / 2;
 let boomY = h / 2;
 
-function boom() {
-  setTimeout(boom, 1000 + random() * 1000);
-
-  shouldBoom = true;
-  setTimeout(() => (shouldBoom = false), 50);
-
-  boomX = w * 0.2 + random() * (w * 0.6);
-  boomY = h * 0.2 + random() * (h * 0.6);
+function onMouseEnter(/* event */) {
+  target.addEventListener('mousemove', onMouseMove);
+  target.addEventListener('mousedown', onMouseDown);
+  target.addEventListener('mouseup', onMouseUp);
 }
-boom();
+
+function onMouseLeave(/* event */) {
+  target.removeEventListener('mousemove', onMouseMove);
+  target.removeEventListener('mousedown', onMouseDown);
+  target.removeEventListener('mouseup', onMouseUp);
+
+  shouldBoom = false;
+  boomX = w / 2;
+  boomY = h / 2;
+}
+
+function onMouseMove({ offsetX, offsetY }) {
+  boomX = offsetX;
+  boomY = offsetY;
+}
+
+function onMouseDown() {
+  shouldBoom = true;
+}
+
+function onMouseUp() {
+  shouldBoom = false;
+}
+
+target.addEventListener('mouseenter', onMouseEnter);
+target.addEventListener('mouseleave', onMouseLeave);
 
 export function getState(/* currentTime, deltaTime */) {
   return {
